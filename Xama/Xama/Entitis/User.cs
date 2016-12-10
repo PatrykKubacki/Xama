d189fd9dd66a7c4dc5409dad325c858d583668bf
patyk <patyk@DESKTOP-RHDUJH3>
2016-12-10 22:15:41 +0100

    Poprawa widokow

--- Xama/Xama/Entitis/User.cs
+++ Xama/Xama/Entitis/User.cs
@@ -5,11 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Xama.Entitis {
-	class User {
+	public class User {
 
 		public int Id { get; set; }
 
 		public string Login { get; set; }
 		public string Password { get; set; }
+
+		public User(int id, string login, string password) {
+			Id = id;
+			Login = login;
+			Password = password;
+		}
 	}
 }
--- Xama/Xama/Pages/HomePage.xaml.cs
+++ Xama/Xama/Pages/HomePage.xaml.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xama.Entitis;
 using Xamarin.Forms;
 
 namespace Xama.Pages {
 	public partial class HomePage : ContentPage {
+
+		public List<User> Users { get; set; }
+
 		public HomePage() {
 			InitializeComponent();
+			Users = new List<User> {
+				new User(1,"admin","admin"),
+			};
 		}
 
-	    private void Login(object sender, EventArgs e)
-	    {
-	        throw new NotImplementedException();
+	    private async void Login(object sender, EventArgs e) {
+		    await Navigation.PushAsync(new TasksPage());
 	    }
+
+		
 	}
 }
