704b62e4347230dbe2ea40ffe0548e13f8c8d026
patyk <patyk@DESKTOP-RHDUJH3>
2016-12-10 20:26:25 +0100

    Dodano encje + strone glowna

--- Xama/Xama/App.cs
+++ Xama/Xama/App.cs
@@ -2,24 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Xama.Pages;
 using Xamarin.Forms;
 
 namespace Xama {
 	public class App : Application {
 		public App() {
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new NavigationPage(new HomePage());
 		}
 
 		protected override void OnStart() {
---
+++ Xama/Xama/Entitis/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xama.Entitis {
+
+	class Task {
+
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+		//TODO Zmien typ na enuma
+		//public Priority Priority { get; set; }
+		public string Priority { get; set; }
+		public DateTime DeadLine { get; set; }
+		public string Category { get; set; }
+		public string Comment { get; set; }
+		public bool Done { get; set; }
+
+		public override string ToString() {
+			return $"{Title}-{Done}";
+		}
+	}
+
+	//TODO Zmien typ na enuma
+	/*
+		[Flags]
+		enum Priority {
+			Undefine = 0,
+			Low = 1,
+			Medium = 2,
+			High= 4,	
+		}
+	*/
+}
